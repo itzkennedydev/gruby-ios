5a9912fcfd04b08f99659191ae2c840fc535e8de
Kennedy Maombi <kennedy@itskennedy.dev>
2025-07-31 12:59:14 -0500

    Initial Commit

---
+++ gruby/grubyApp.swift
@@ -0,0 +1,17 @@
+//
+//  grubyApp.swift
+//  gruby
+//
+//  Created by Kennedy Maombi on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct grubyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
